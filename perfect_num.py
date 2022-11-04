ffeee3425806991fc0e52c6e45e4ef050e80619e
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-04 22:43:02 +0530

    Delete perfect_num.py

--- perfect_num.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter the number :\n"))
-sum = 0
-for i in range(1,num):
-    if num % i == 0:
-        sum+=i
-if sum == num:
-    print(f"{num} is a perfect number")
-else:
-    print(f"{num} is not a perfect number")
