a60966e99b6553c0d5ad21e9ca8d99b8c27333b3
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-10 08:16:02 +0530

    Initial Python Coding

---
+++ function01.py
@@ -0,0 +1,7 @@
+def myself():
+    print('''My Name Is Vaibhav Agrawal
+I Am From Gorakhpur
+I Am High School Graduate With 89.8%
+I Qualified My Intermediate With 88.6%
+Currently I Am Pursuing Btech From GLA University''')
+myself()
