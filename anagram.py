8b3ef11dcce41a6ec90fc941f8ef3bb6031329a0
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-12 09:51:59 +0530

    ANAGRAM Check

--- anagram.py
+++ anagram.py
@@ -1,5 +1,5 @@
-str1 = input("Enter the First word: \n")
-str2 = input("Enter the Second word: \n")
+str1 = input("Enter the First word: \n").upper()
+str2 = input("Enter the Second word: \n").upper()
 if sorted(str1) == sorted(str2):
     print(f"{str1} and {str2} are anagram")
 else:
