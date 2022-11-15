e3e4e1c763cdbca2aeea244a08a943b849576135
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-15 13:07:54 +0530

    Initial Python Coding

---
+++ star_pattern06.py
@@ -0,0 +1,19 @@
+n = int(input("Enter The Number of Lines you want :"))
+for i in range(1,n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(1,i+1):
+        if (j==i or j==1):
+            print("$ ", end="")
+        else:
+            print("  ",end="")    
+    print()
+for i in range(1,n+1):
+    for j in range(1,i):
+        print(" ",end="")
+    for j in range(i,n+1):
+        if (j==i or j==n):
+            print("$ ",end="")
+        else:
+            print("  ",end="")
+    print()
