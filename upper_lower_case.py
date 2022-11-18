1852441bfe0c539a2d5300edd86e46bf0895725e
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-19 01:47:28 +0530

    Initial Python Learning

--- upper_lower_case.py
+++ upper_lower_case.py
@@ -1,4 +1,4 @@
-str = input("Enter your string in lower or upper string :\n")
+str = input("Enter your string in lower or upper string : ")
 if str == str.upper():
     print("The string is in upper case")
 elif str == str.lower():
