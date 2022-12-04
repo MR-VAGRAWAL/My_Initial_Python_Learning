0234d4049c5ac41c634a45bacfb9d412be3766ba
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-12-05 00:19:32 +0530

    Functions in Math Module

---
+++ math_function.py
@@ -0,0 +1,18 @@
+import math
+a = 3.3
+num = 7
+print(math.ceil(a))
+print(math.floor(a)) 
+print(math.factorial(num)) 
+num1 = 21
+num2 = 3
+print(math.gcd(21 , 3))
+num3 = -17
+print(math.fabs(num3))
+print(math.pow(num2 , num))
+print(math.sqrt(num2))
+c = math.pi/7
+d = 17
+print(math.degrees(c))
+print(math.radians(d))
+print(math.log(d,10))
