2d9992dc1a2001581e25ed93f5535a49f96b5308
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-12-19 00:00:59 +0530

    Initial Python Coding

--- table.py
+++ table.py
@@ -1,4 +1,4 @@
-# Create a multiplication table by taking input from the user
+# Create a multiplication table by taking input from the user.
 num = int(input("Enter the number : \n"))
 for i in range(1,11):
     print(f"{num} * {i} = {num*i}")
