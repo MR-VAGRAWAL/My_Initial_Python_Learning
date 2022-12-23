dd9f16a0e063f9e45aef5e7930ac1b7c954d42cd
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-12-23 20:36:43 +0530

    Function To Check The Function

---
+++ matrix_check.py
@@ -0,0 +1,13 @@
+ls = [[1,2,3],[4,5,6],[7,8,9]]
+def my_matrix(ls1):
+    n = len(ls1[0])
+    for i in ls1:
+        if len(i) != n:
+            return False
+    return True
+    
+if my_matrix(ls):
+    for i in ls:
+        print(*i,sep = "\t")
+else:
+    print("Invalid Matrix")
